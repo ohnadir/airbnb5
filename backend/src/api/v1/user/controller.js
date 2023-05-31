b8ecda42fe98b9551f0beb184a462e6a34b5c949
Nadir Hossain <nadirhossain336@gmail.com>
2023-05-31 14:31:45 +0600

    create controller for user api

---
+++ backend/src/api/v1/user/controller.js
@@ -0,0 +1,29 @@
+const {
+    registration,
+    login,
+    loadUser,
+  } =require("./service")
+
+
+exports.Register = async (req, res) => {
+    const { status, code, message, success, token, user } = await registration({
+      body:req.body,
+      ...req.body
+    });
+    res.status(code).json({ code, status, message, success, token, user });
+};
+  
+exports.Login = async (req, res) => {
+    const { status, code, message, success, token, user } = await login({
+      body:req.body,
+      ...req.body
+    });
+    res.status(code).json({ code, status, message,  success, token, user });
+};
+
+exports.LoadUser = async (req, res) => {
+    const { status, code, message, user } = await loadUser({
+      token:req.params.token
+    });
+    res.status(code).json({ code, status, message, user });
+};
