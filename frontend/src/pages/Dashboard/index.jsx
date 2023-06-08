32ee335f9aaada8bc66a72d5f7636bbd3022a074
Nadir Hossain <nadirhossain336@gmail.com>
2023-06-08 09:01:41 +0600

    booking status implement into the dashboard

--- frontend/src/pages/Dashboard/index.jsx
+++ frontend/src/pages/Dashboard/index.jsx
@@ -40,6 +40,70 @@ const Dashboard = () => {
                     </div>
                 </div>
             </div>
+            <div className="booking-status">
+                <div className='booking-item'>
+                    <div className='booking-icon cart-icon'>
+                    <span>
+                        <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle>
+                        <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
+                        </svg>
+                    </span>
+                    </div>
+                    <div>
+                    <p className='counter-text'>Total Booking</p>
+                    <p className='counter-number'>
+                        <CountUp end={20} />
+                    </p>
+                    </div>
+                </div>
+                <div className='booking-item'>
+                    <div className='booking-icon pending-icon'>
+                    <span>
+                        <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><polyline points="23 4 23 10 17 10"></polyline><polyline points="1 20 1 14 7 14"></polyline>
+                        <path d="M3.51 9a9 9 0 0 1 14.85-3.36L23 10M1 14l4.64 4.36A9 9 0 0 0 20.49 15"></path>
+                        </svg>
+                    </span>
+                    </div>
+                    <div>
+                    <p className='counter-text'>Pending Booking</p>
+                    <p className='counter-number'>
+                        <CountUp end={18} />
+                    </p>
+                    </div>
+                </div>
+                <div className='booking-item'>
+                    <div className='booking-icon process-icon'>
+                    <span>
+                        <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                        <rect x="1" y="3" width="15" height="13"></rect>
+                        <polygon points="16 8 20 8 23 11 23 16 16 16 16 8"></polygon>
+                        <circle cx="5.5" cy="18.5" r="2.5"></circle><circle cx="18.5" cy="18.5" r="2.5"></circle>
+                        </svg>
+                    </span>
+                    </div>
+                    <div>
+                    <p className='counter-text'>Processing Booking</p>
+                    <p className='counter-number'>
+                        <CountUp end={11} />
+                    </p>
+                    </div>
+                </div>
+                <div className='booking-item'>
+                    <div className='booking-icon complete-icon'>
+                    <span>
+                        <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                        <polyline points="20 6 9 17 4 12"></polyline>
+                        </svg>
+                    </span>
+                    </div>
+                    <div>
+                    <p className='counter-text'>Complete Booking</p>
+                    <p className='counter-number'>
+                        <CountUp end={29} />
+                    </p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
   )
