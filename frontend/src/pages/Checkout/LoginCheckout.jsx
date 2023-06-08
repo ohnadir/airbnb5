8c1ae27e09b46083275a0d061898220201daed27
Nadir Hossain <nadirhossain336@gmail.com>
2023-06-09 01:53:27 +0600

    checkout authentication successfully

--- frontend/src/pages/Checkout/LoginCheckout.jsx
+++ frontend/src/pages/Checkout/LoginCheckout.jsx
@@ -1,13 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Checkout.scss"
 import RegisterCheckout from "./RegisterCheckout";
-
+import { message } from 'antd';
+import { useDispatch, useSelector } from "react-redux"
+import { login } from "../../Redux/actions/user";
 
 const AuthCheckout = () => {
+    const [messageApi, contextHolder] = message.useMessage();
+    const { user, error } = useSelector(state => state.auth)
     const [input, setInput] = useState("");
     const [passInput, setPassInput] = useState("");
     const [password, setPassword] = useState("")
     const [isValid, setIsValid] = useState(false);
+    const dispatch = useDispatch();
+
     const google=()=> {
         window.open("http://localhost:5001/auth/google",  "_blank", 
         " width=500,height=500");
@@ -20,85 +26,114 @@ const AuthCheckout = () => {
         window.open("http://localhost:5001/auth/facebook", "_blank", 
         " width=500,height=500");
     };
+
+    // check valid email or not 
+    const validateEmail = (email) => {
+        const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+        return regex.test(email);
+    };
+    
+    const auth  = {
+        email: input,
+        password: passInput
+    }
     const handleSubmit = () => {
-        if(input){
+        const result = validateEmail(input)
+        if(result){
             setPassword(true)
         }
-        if(password){
-            setIsValid(true)
+        else{
+            messageApi.error("Invalid Email");
+        }
+        if(passInput){
+            dispatch(login(auth))
         }
     }
+    useEffect(()=>{
+        if (user?.email) {
+            messageApi.success("login successfully");
+        }
+        if(error === "Incorrect credential"){
+            messageApi.warning("Incorrect credential");
+        }
+        if(error === "user not found"){
+            setIsValid(true);
+        }
+    },[error, user])
     return (
-        <div className='auth-checkout'>
-            {
-                isValid 
-                ?
-                <RegisterCheckout input={input} passInput={passInput}/>
-                :
-                <div>
-                    <section className="">
-                        <h1 className='text-2xl mb-5'>Log in or sign up to book</h1>
-                        <div className="auth-container">
-                            {
-                                password 
-                                ? 
-                                <div className="input-container" >
-                                    <div className="">
-                                        <label className='relative cursor-pointer'>
-                                            <input value={passInput} type="password" onChange={(e)=>setPassInput(e.target.value)} placeholder="xxxx xxxx xxx" className='pt-5 pb-3 w-full px-[14px] text-[14px]  border-none rounded-lg border-opacity-50 outline-none  placeholder-gray-300 placeholder-opacity-0 transition duration-200' />
-                                            <span className='text-opacity-80  absolute left-[21px] top-[30%] text-black translate-y-[-50%]  transition duration-200 input-password'>Password</span>
-                                        </label>
+        <>
+            {contextHolder}
+            <div className='auth-checkout'>
+                {
+                    isValid 
+                    ?
+                    <RegisterCheckout input={input} passInput={passInput}/>
+                    :
+                    <div>
+                        <section className="">
+                            <h1 className='text-2xl mb-5'>Log in or sign up to book</h1>
+                            <div className="auth-container">
+                                {
+                                    password 
+                                    ? 
+                                    <div className="input-container" >
+                                        <div className="">
+                                            <label className='relative cursor-pointer'>
+                                                <input value={passInput} type="password" onChange={(e)=>setPassInput(e.target.value)} placeholder="xxxx xxxx xxx" className='pt-5 pb-3 w-full px-[14px] text-[14px]  border-none rounded-lg border-opacity-50 outline-none  placeholder-gray-300 placeholder-opacity-0 transition duration-200' />
+                                                <span className='text-opacity-80  absolute left-[21px] top-[30%] text-black translate-y-[-50%]  transition duration-200 input-password'>Password</span>
+                                            </label>
+                                        </div>
                                     </div>
-                                </div>
-                                :
-                                <div className="input-container" >
-                                    <div className="">
-                                        <label className='relative cursor-pointer'>
-                                            <input type="text" onChange={(e)=>setInput(e.target.value)} placeholder="xxxx xxxx xxx" className='pt-5 pb-3 w-full px-[14px] text-[14px]  border-none rounded-lg border-opacity-50 outline-none  placeholder-gray-300 placeholder-opacity-0 transition duration-200' />
-                                            <span className='text-opacity-80  absolute left-[20px] top-[30%] text-black translate-y-[-50%]  transition duration-200 input-email'>Email</span>
-                                        </label>
+                                    :
+                                    <div className="input-container" >
+                                        <div className="">
+                                            <label className='relative cursor-pointer'>
+                                                <input type="text" onChange={(e)=>setInput(e.target.value)} placeholder="xxxx xxxx xxx" className='pt-5 pb-3 w-full px-[14px] text-[14px]  border-none rounded-lg border-opacity-50 outline-none  placeholder-gray-300 placeholder-opacity-0 transition duration-200' />
+                                                <span className='text-opacity-80  absolute left-[20px] top-[30%] text-black translate-y-[-50%]  transition duration-200 input-email'>Email</span>
+                                            </label>
+                                        </div>
                                     </div>
-                                </div>
-                            } 
+                                } 
+                            </div>
+                            <span className='text-xs'>We&apos;ll call or text you to confirm your number. Standard message and data rates apply. <span className='underline font-bold'>Privacy Policy</span></span>    
+                        </section>
+                        
+                        {/* login button start */}
+                        <div className="btn mt-4">
+                            <button disabled={!input} onClick={handleSubmit} className="auth-btn">Continue</button>
                         </div>
-                        <span className='text-xs'>We&apos;ll call or text you to confirm your number. Standard message and data rates apply. <span className='underline font-bold'>Privacy Policy</span></span>    
-                    </section>
-                    
-                    {/* login button start */}
-                    <div className="btn mt-4">
-                        <button disabled={!input} onClick={handleSubmit} className="auth-btn">Continue</button>
+
+                        {/* divider start */}
+                        <section className="my-5">
+                            <div className='flex items-center gap-6'>
+                                <div className="card-divider"></div>
+                                <div>or</div>
+                                <div className="card-divider"></div>
+                            </div>
+                        </section>
+
+                        {/* social icon section  start*/}
+                        <section className="social-auth">
+                            <div className='social-icon-container'>
+                                <div onClick={facebook}  className="social-icon">
+                                    <img className="icon" src="https://res.cloudinary.com/dcmvctoz3/image/upload/v1666711218/facebook_iukumb.png" alt="" />
+                                </div>
+                                <div onClick={google} className="social-icon">
+                                    <img  className="icon" src="https://res.cloudinary.com/dcmvctoz3/image/upload/v1666711180/search_nlfgqn.png" alt="" />
+                                </div>
+                                <div onClick={github} className="social-icon">
+                                    <img className="icon" src="https://res.cloudinary.com/dcmvctoz3/image/upload/v1666717428/apple_ouqww9.png" alt="" />
+                                </div>
+                            </div>
+                            <div className="social-icon full-icon">
+                                <img className="icon mx-auto" src="https://res.cloudinary.com/dcmvctoz3/image/upload/v1666717428/email_i5xejg.png" alt="" />
+                                <p className='icon-text m-0'>Continue with email</p>
+                            </div>
+                        </section>
                     </div>
-
-                    {/* divider start */}
-                    <section className="my-5">
-                        <div className='flex items-center gap-6'>
-                            <div className="card-divider"></div>
-                            <div>or</div>
-                            <div className="card-divider"></div>
-                        </div>
-                    </section>
-
-                    {/* social icon section  start*/}
-                    <section className="social-auth">
-                        <div className='social-icon-container'>
-                            <div onClick={facebook}  className="social-icon">
-                                <img className="icon" src="https://res.cloudinary.com/dcmvctoz3/image/upload/v1666711218/facebook_iukumb.png" alt="" />
-                            </div>
-                            <div onClick={google} className="social-icon">
-                                <img  className="icon" src="https://res.cloudinary.com/dcmvctoz3/image/upload/v1666711180/search_nlfgqn.png" alt="" />
-                            </div>
-                            <div onClick={github} className="social-icon">
-                                <img className="icon" src="https://res.cloudinary.com/dcmvctoz3/image/upload/v1666717428/apple_ouqww9.png" alt="" />
-                            </div>
-                        </div>
-                        <div className="social-icon full-icon">
-                            <img className="icon mx-auto" src="https://res.cloudinary.com/dcmvctoz3/image/upload/v1666717428/email_i5xejg.png" alt="" />
-                            <p className='icon-text m-0'>Continue with email</p>
-                        </div>
-                    </section>
-                </div>
-            }
-        </div>
+                }
+            </div>
+        </>
     )
 }
 
--- frontend/src/pages/Checkout/index.jsx
+++ frontend/src/pages/Checkout/index.jsx
@@ -24,6 +24,7 @@ const options = {
 const stripePromise = loadStripe("pk_test_51MJynOHzN4rqAg27o1nDk5hQeHaX8cuaBkInxAzGMEnEqee4QMyeztVLqyeuAhzgK9ZRdwPAF8uWFrRX2Qj8iuQ9005XC9m0sA");
 const Checkout = () => {
     const { place, loading } = useSelector(state=> state.place);
+    const { isAuthenticated} = useSelector(state => state.auth);
     const dispatch = useDispatch();
     const id = "64770dde9c9a8f27aee50f5c"
     useEffect(()=>{
@@ -110,87 +111,93 @@ const Checkout = () => {
                                 </div>
                             </section>
                             <div className="card-divider my-5"></div>
-
-                            {/* payment section start */}
-                            <section className="payment-section">
-                                <div className='payment-heading'>
-                                    <h1 className="text-[18px] font-semibold">Pay With</h1>
-                                    <div className="flex items-center gap-3">
-                                        <img className="w-9" src="https://a0.muscache.com/airbnb/static/packages/assets/frontend/legacy-shared/svgs/payments/logo_visa.0adea522bb26bd90821a8fade4911913.svg" alt=""/>
-                                        <img className="w-7" src="https://a0.muscache.com/airbnb/static/packages/assets/frontend/legacy-shared/svgs/payments/logo_mastercard.f18379cf1f27d22abd9e9cf44085d149.svg" alt=""/>
-                                    </div>
-                                </div>
-                                <div className='border m-0 p-3 rounded-[4px]'>
-                                    <Elements stripe={stripePromise}>
-                                        <CardElement options={options}/>
-                                    </Elements>
-                                </div>
-                            </section>
-                            {/* Billing address start */}
-                            <section className="billing-address">
-                                <h1 className=" mb-5 mt-6 font-semibold">Billing Address</h1>
-                                <div className="billing-address-container border rounded-[8px]">
-                                    <div className="relative" style={{borderBottom:"1px solid #B0B0B0"}}>
-                                        <label className='relative cursor-pointer'>
-                                            <input name='address'  type="text" placeholder="Input" className='pt-5 pb-3 w-full px-[14px] text-[14px]  border-none rounded-lg border-opacity-50 outline-none  placeholder-gray-300 placeholder-opacity-0 transition duration-200' />
-                                            <span className='text-opacity-80  absolute left-[14px] top-[30%] text-black translate-y-[-50%] text-[15px]  transition duration-200 input-text'>Street Address</span>
-                                        </label>
-                                    </div>
-                                    <div className="relative" style={{borderBottom:"1px solid #B0B0B0"}}>
-                                        <label className='relative cursor-pointer'>
-                                            <input name='aptNumber'  type="text" placeholder="Input" className='pt-5 pb-3 w-full px-[14px] text-[14px]  border-none rounded-lg border-opacity-50 outline-none  placeholder-gray-300 placeholder-opacity-0 transition duration-200' />
-                                            <span className='text-opacity-80  absolute left-[14px] top-[30%] text-black translate-y-[-50%] text-[15px] transition duration-200 input-text-a'>Apt or suite number</span>
-                                        </label>
-                                    </div>
-                                    <div className="" style={{borderBottom:"1px solid #B0B0B0"}}>
-                                        <label className='relative cursor-pointer'>
-                                            <input name='city'  type="text" placeholder="Input" className='pt-5 pb-3 w-full px-[14px] text-[14px]  border-none rounded-lg border-opacity-50 outline-none  placeholder-gray-300 placeholder-opacity-0 transition duration-200' />
-                                            <span className='text-opacity-80  absolute left-[14px] top-[30%] text-black translate-y-[-50%] text-[15px] transition duration-200 input-text-c'>City</span>
-                                        </label>
-                                    </div>
-                                    <div className="flex ">
-                                        <div className="w-full">
-                                            <label className='relative cursor-pointer'>
-                                                <input name='state'  type="text" placeholder="Input" className='pt-5 pb-3 w-full px-[14px] text-[14px]  border-none rounded-lg border-opacity-50 outline-none  placeholder-gray-300 placeholder-opacity-0 transition duration-200' />
-                                                <span className='text-opacity-80  absolute left-[14px] top-[30%] text-black translate-y-[-50%] text-[15px] transition duration-200 input-text-z'>State</span>
-                                            </label>
+                            {
+                                isAuthenticated
+                                ?
+                                <>
+                                    {/* payment section start */}
+                                    <section className="payment-section">
+                                        <div className='payment-heading'>
+                                            <h1 className="text-[18px] font-semibold">Pay With</h1>
+                                            <div className="flex items-center gap-3">
+                                                <img className="w-9" src="https://a0.muscache.com/airbnb/static/packages/assets/frontend/legacy-shared/svgs/payments/logo_visa.0adea522bb26bd90821a8fade4911913.svg" alt=""/>
+                                                <img className="w-7" src="https://a0.muscache.com/airbnb/static/packages/assets/frontend/legacy-shared/svgs/payments/logo_mastercard.f18379cf1f27d22abd9e9cf44085d149.svg" alt=""/>
+                                            </div>
                                         </div>
-                                        <div className="w-full" style={{borderLeft:"1px solid #B0B0B0"}}>
-                                            <label className='relative cursor-pointer'>
-                                                <input name='zipCode'  type="text" placeholder="Input" className='pt-5 pb-3 w-full px-[14px] text-[14px]  border-none rounded-lg border-opacity-50 outline-none  placeholder-gray-300 placeholder-opacity-0 transition duration-200' />
-                                                <span className='text-opacity-80  absolute left-[14px] top-[30%] text-black translate-y-[-50%] text-[14px] transition duration-200 input-text-z'>ZIP Code</span>
-                                            </label>
+                                        <div className='border m-0 p-3 rounded-[4px]'>
+                                            <Elements stripe={stripePromise}>
+                                                <CardElement options={options}/>
+                                            </Elements>
                                         </div>
-                                    </div>
-                                </div>
+                                    </section>
+                                    {/* Billing address start */}
+                                    <section className="billing-address">
+                                        <h1 className=" mb-5 mt-6 font-semibold">Billing Address</h1>
+                                        <div className="billing-address-container border rounded-[8px]">
+                                            <div className="relative" style={{borderBottom:"1px solid #B0B0B0"}}>
+                                                <label className='relative cursor-pointer'>
+                                                    <input name='address'  type="text" placeholder="Input" className='pt-5 pb-3 w-full px-[14px] text-[14px]  border-none rounded-lg border-opacity-50 outline-none  placeholder-gray-300 placeholder-opacity-0 transition duration-200' />
+                                                    <span className='text-opacity-80  absolute left-[14px] top-[30%] text-black translate-y-[-50%] text-[15px]  transition duration-200 input-text'>Street Address</span>
+                                                </label>
+                                            </div>
+                                            <div className="relative" style={{borderBottom:"1px solid #B0B0B0"}}>
+                                                <label className='relative cursor-pointer'>
+                                                    <input name='aptNumber'  type="text" placeholder="Input" className='pt-5 pb-3 w-full px-[14px] text-[14px]  border-none rounded-lg border-opacity-50 outline-none  placeholder-gray-300 placeholder-opacity-0 transition duration-200' />
+                                                    <span className='text-opacity-80  absolute left-[14px] top-[30%] text-black translate-y-[-50%] text-[15px] transition duration-200 input-text-a'>Apt or suite number</span>
+                                                </label>
+                                            </div>
+                                            <div className="" style={{borderBottom:"1px solid #B0B0B0"}}>
+                                                <label className='relative cursor-pointer'>
+                                                    <input name='city'  type="text" placeholder="Input" className='pt-5 pb-3 w-full px-[14px] text-[14px]  border-none rounded-lg border-opacity-50 outline-none  placeholder-gray-300 placeholder-opacity-0 transition duration-200' />
+                                                    <span className='text-opacity-80  absolute left-[14px] top-[30%] text-black translate-y-[-50%] text-[15px] transition duration-200 input-text-c'>City</span>
+                                                </label>
+                                            </div>
+                                            <div className="flex ">
+                                                <div className="w-full">
+                                                    <label className='relative cursor-pointer'>
+                                                        <input name='state'  type="text" placeholder="Input" className='pt-5 pb-3 w-full px-[14px] text-[14px]  border-none rounded-lg border-opacity-50 outline-none  placeholder-gray-300 placeholder-opacity-0 transition duration-200' />
+                                                        <span className='text-opacity-80  absolute left-[14px] top-[30%] text-black translate-y-[-50%] text-[15px] transition duration-200 input-text-z'>State</span>
+                                                    </label>
+                                                </div>
+                                                <div className="w-full" style={{borderLeft:"1px solid #B0B0B0"}}>
+                                                    <label className='relative cursor-pointer'>
+                                                        <input name='zipCode'  type="text" placeholder="Input" className='pt-5 pb-3 w-full px-[14px] text-[14px]  border-none rounded-lg border-opacity-50 outline-none  placeholder-gray-300 placeholder-opacity-0 transition duration-200' />
+                                                        <span className='text-opacity-80  absolute left-[14px] top-[30%] text-black translate-y-[-50%] text-[14px] transition duration-200 input-text-z'>ZIP Code</span>
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        </div>
 
-                                {/* country name modal start */}
-                                <div className="country-name border rounded-[8px] mt-5">
-                                    <div>
-                                        <p className=''>Country/Region</p>
-                                        <h3>Bangladesh</h3>
-                                    </div>
-                                    <FiChevronDown size={18} />
-                                </div>
-                            </section>
-                            <div className="card-divider my-8"></div>
+                                        {/* country name modal start */}
+                                        <div className="country-name border rounded-[8px] mt-5">
+                                            <div>
+                                                <p className=''>Country/Region</p>
+                                                <h3>Bangladesh</h3>
+                                            </div>
+                                            <FiChevronDown size={18} />
+                                        </div>
+                                    </section>
+                                    <div className="card-divider my-8"></div>
 
-                            {/* cancellation policy term start */}
-                            <section className="cancellation-policy">
-                                <h1 className="heading">Cancellation Policy</h1>
-                                <p className="m-0 text-[15px]">
-                                    <span className="font-bold">Free cancellation for 48 hours. </span> 
-                                    Cancel before Dec 24 for a partial refund. 
-                                    <span className="font-bold underline">Learn more</span>
-                                </p>
-                                <div className="card-divider my-5"></div>
-                                <p className="m-0 text-[13px] leading-5 mb-6">By selecting the button below, I agree to the Host&apos;s House Rules, Airbnb&apos;s Rebooking and Refund Policy, and that Airbnb can charge my payment method if I&apos;m responsible for damage. I agree to pay the total amount shown if the Host accepts my booking request.</p>
-                            </section>
+                                    {/* cancellation policy term start */}
+                                    <section className="cancellation-policy">
+                                        <h1 className="heading">Cancellation Policy</h1>
+                                        <p className="m-0 text-[15px]">
+                                            <span className="font-bold">Free cancellation for 48 hours. </span> 
+                                            Cancel before Dec 24 for a partial refund. 
+                                            <span className="font-bold underline">Learn more</span>
+                                        </p>
+                                        <div className="card-divider my-5"></div>
+                                        <p className="m-0 text-[13px] leading-5 mb-6">By selecting the button below, I agree to the Host&apos;s House Rules, Airbnb&apos;s Rebooking and Refund Policy, and that Airbnb can charge my payment method if I&apos;m responsible for damage. I agree to pay the total amount shown if the Host accepts my booking request.</p>
+                                    </section>
 
-                            {/* submit button */}
-                            <button   className="confirm-btn">Confirm and pay</button>
+                                    {/* submit button */}
+                                    <button   className="confirm-btn">Confirm and pay</button>
+                                </>
+                                :
+                                <AuthCheckout/>
+                            }
                         </div>
-                        <AuthCheckout/>
                     </div>
                 </div>
             }
