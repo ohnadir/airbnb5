a0d04f8c6b3086035371676f4aabb21330915126
Nadir Hossain <nadirhossain336@gmail.com>
2023-06-08 22:54:34 +0600

    checkout card implementation

--- frontend/src/pages/Checkout/index.jsx
+++ frontend/src/pages/Checkout/index.jsx
@@ -22,23 +22,59 @@ const Checkout = () => {
                 :
                 <div className='checkout'>
                     <h1 className='heading'>Confirm and Pay</h1>
-                    {/* this card for mobile and tablet version */}
 
+                    {/* this card for mobile and tablet version */}
                     <div className='mobile-place-card'>
                         <img  src="https://a0.muscache.com/im/pictures/c1fa3691-1287-4334-981d-ef9a9a8f5a56.jpg?im_w=720" alt="" />
-                        <div className="w-1/2 flex flex-col justify-between">
+                        <div className="place-info">
                             <div>
-                                <p style={{color:"#696969"}} className="m-0 text-xs">{place?.name}</p>
-                                <p className="m-0 mt-[-5px] text-[16px]">{place?.des}</p>
+                                <p className="place-name">{place?.name}</p>
+                                <p className="place-des">{place?.des}</p>
                             </div>
                             <div className="flex items-center gap-1  text-[13px]">
-                                <span className="flex items-center gap-1 "><FaStar/>{place?.rating}</span>
+                                <span className="flex items-center gap-1 ">
+                                    <FaStar/>{place?.rating}
+                                </span>
                                 <span className=''>(17)</span>
                             </div>
                         </div>
                     </div>
 
                     <div className="checkout-container">
+                        <div className="checkout-card">
+                            <div className='place-card'>
+                                <img  src="https://a0.muscache.com/im/pictures/c1fa3691-1287-4334-981d-ef9a9a8f5a56.jpg?im_w=720" alt="" />
+                                <div className="place-info">
+                                    <div>
+                                        <p className="place-name">{place?.name}</p>
+                                        <p className="place-des">{place?.des}</p>
+                                    </div>
+                                    <div className="flex items-center gap-1  text-[13px]">
+                                        <span className="flex items-center gap-1 ">
+                                            <FaStar/>{place?.rating}
+                                        </span>
+                                        <span className=''>(17)</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card-divider my-5"></div>
+                            <h2 className='price-heading'>Price details</h2>
+                            <div className="mt-3 grid grid-cols-1 gap-2">
+                                <div className='price-container'>
+                                    <span>$56.24 x 7 nights</span>
+                                    <span>$393.67</span>
+                                </div>
+                                <div className='price-container'>
+                                    <span className="text-[#696969]">Service fee</span>
+                                    <span className="text-[#54B157]">$55.58</span>
+                                </div>
+                                <div className="card-divider my-4"></div>
+                                <div className='price-container total-price'>
+                                    <span>Total </span>
+                                    <span>$496.49</span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             }
