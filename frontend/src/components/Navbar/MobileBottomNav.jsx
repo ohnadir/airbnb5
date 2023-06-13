73fb19140fb5b29572f748cba92c1b5c878de589
Nadir Hossain <nadirhossain336@gmail.com>
2023-06-13 12:01:48 +0600

    mobile bottom navbar style modify

--- frontend/src/components/Navbar/MobileBottomNav.jsx
+++ frontend/src/components/Navbar/MobileBottomNav.jsx
@@ -3,9 +3,12 @@ import './Navbar.scss'
 import { FiUser } from 'react-icons/fi'
 import { BiSearch } from 'react-icons/bi'
 import { IoMdHeartEmpty } from 'react-icons/io';
+import { FaAirbnb } from 'react-icons/fa';
+import { useSelector } from "react-redux"
 
 const MobileBottomNav = () => {
     const [show, setShow] = useState(false);
+    const { isAuthenticated } = useSelector(state=>state.auth)
     const controlNavbar = () => {
         const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
         if(scrollTop){
@@ -26,18 +29,52 @@ const MobileBottomNav = () => {
     return (
         <div className='mobile-bottom-nav' style={{height: show ? "65px" : 0}}>
             <div className="mobile-bottom-nav-container">
-                <div className='nav-item'>
-                    <BiSearch className='icon'/>
+                <div className='nav-item' tabIndex="1">
+                    <BiSearch className='icon icons'/>
                     <p>Explore</p>
                 </div>
-                <div className='nav-item'>
-                    <IoMdHeartEmpty className='icon'/>
-                    <p>Wishlists</p>
-                </div>
-                <div className='nav-item'>
-                    <FiUser className='icon'/>
-                    <p>Log in</p>
-                </div>
+                {
+                    isAuthenticated
+                    ?
+                    <div className='nav-item' tabIndex="2">
+                        <IoMdHeartEmpty className='icon'/>
+                        <p>Wishlists</p>
+                    </div>
+                    :
+                    null
+                }
+                {
+                    isAuthenticated
+                    ?
+                    <div className='nav-item' tabIndex="3">
+                        <FaAirbnb className='icon'/>
+                        <p>Trips</p>
+                    </div>
+                    :
+                    null
+                }
+                {
+                    isAuthenticated
+                    ?
+                    null
+                    :
+                    <div className='nav-item' tabIndex="4">
+                        <FiUser className='icon'/>
+                        <p>Log in</p>
+                    </div>
+                }
+                
+                {
+                    isAuthenticated
+                    ?
+                    <div className='nav-item' tabIndex="5">
+                        <FiUser className='icon'/>
+                        <p>Profile</p>
+                    </div>
+                    :
+                    null
+                }
+                
             </div>
         </div>
     )
