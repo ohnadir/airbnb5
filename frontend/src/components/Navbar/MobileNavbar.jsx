c4e3215519af0175764330c4680cb98d36953b4c
Nadir Hossain <nadirhossain336@gmail.com>
2023-07-06 11:49:24 +0600

    mobile navbar implement

--- frontend/src/components/Navbar/MobileNavbar.jsx
+++ frontend/src/components/Navbar/MobileNavbar.jsx
@@ -3,10 +3,25 @@ import './Navbar.scss'
 import { BiSearch } from 'react-icons/bi'
 import { CgOptions } from 'react-icons/cg'
 import { MdClose } from 'react-icons/md'
+import { TbSearch } from 'react-icons/tb';
+import Region from "../../JSON/Area.json"
+import { useNavigate } from 'react-router-dom';
+import { format, differenceInDays  } from "date-fns";
+import { DateRange } from "react-date-range";
+import FilterModal from '../Category/FilterModal'
+import Slider from "react-slick";
 
 const MobileNavbar = () => {
+    const [open1, setOpen1] = useState(false);
     const [open, setOpen] = useState(false);
     const [item, setItem] = useState("")
+    const [date, setDate] = useState([
+        {
+        startDate: new Date(), 
+        endDate: new Date(),
+        key: "selection",
+        }
+    ]);
     const [options, setOptions] = useState({
         adult: 0,
         children: 0,
@@ -23,12 +38,52 @@ const MobileNavbar = () => {
     };
     const handleSearch=()=>{
         setItem("")
-        setOpen(false)
+        setOpen1(false)
     }
+    
+      const settings = {
+        dots: false,
+        arrows: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        initialSlide: 0,
+        responsive: [
+          {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 5,
+              slidesToScroll: 0,
+              infinite: true
+            }
+          },
+          {
+            breakpoint: 631,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 1,
+              initialSlide: 2
+            }
+          },
+          {
+            breakpoint: 525,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 420,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          } 
+        ]
+      }
     return (
         <div className='block md:hidden mobile-navbar-container px-5'>
             <div className='mobile-navbar-content'>
-                <div className='mobile-search-label cursor-pointer w-full' onClick={()=>setOpen(true)}>
+                <div className='mobile-search-label cursor-pointer w-full' onClick={()=>setOpen1(true)}>
                     <BiSearch className='text-xl'/>
                     <div className='py-2 ' >
                         <h1 className='m-0 text-[13px] font-semibold'>Anywhere</h1>
@@ -38,15 +93,15 @@ const MobileNavbar = () => {
                         </ul>
                     </div>
                 </div>
-                <CgOptions className='filter-icon' />
+                <CgOptions onClick={()=>setOpen(true)} className='filter-icon' />
             </div>
             {
-                open
+                open1
                 ?
                 <div className='navbar-mask'>
                     <div className='navbar-mask-container'>
                         <div data-aos="fade-down" className='close-icon-container flex items-center'>
-                            <div className='close-icon w-fit' onClick={()=>setOpen(false)}>
+                            <div className='close-icon w-fit' onClick={()=>setOpen1(false)}>
                                 <MdClose size={20}/>
                             </div>
                             <div className='flex mx-auto gap-5 items-center justify-center font-semibold text-gray-600'>
@@ -54,13 +109,30 @@ const MobileNavbar = () => {
                                 <span>Experiences</span>
                             </div>
                         </div>
-                        <div className="mask-content" >
+                        <div className="mask-content">
                             <div data-aos="fade-down" data-aos-delay="200" className='options' onClick={()=>setItem("location")}>
                                 {
                                     item === "location"
                                     ?
-                                    <div>
+                                    <div className='destinations'>
                                         <h1 className='option-heading'>Where to?</h1>
+                                        <div className='search-container flex items-center gap-3'>
+                                            <TbSearch size={20}/>
+                                            <input type="text" placeholder='Search destinations' />
+                                        </div>
+                                        <div className="region-map-container overflow-hidden">
+                                            <Slider {...settings}>
+                                                {
+                                                    Region.map((item, index)=>
+                                                        <div key={index} className='region-map'>
+                                                            <img src={item?.photo} alt="" />
+                                                            <p>{item.name}</p>
+                                                        </div>
+                                                    )
+                                                }
+                                            </Slider>
+                                        </div>
+                                        
                                     </div>
                                     :
                                     <div className='options-item' >
@@ -75,6 +147,14 @@ const MobileNavbar = () => {
                                     ?
                                     <div>
                                         <h1 className='option-heading'>When&apos;s your trip?</h1>
+                                        <div className="calender-container">
+                                            <DateRange
+                                                editableDateInputs={true}
+                                                onChange={item => setDate([item.selection])}
+                                                moveRangeOnFirstSelection={false}
+                                                ranges={date}
+                                             />
+                                        </div>
                                     </div>
                                     :
                                     <div className='options-item' >
@@ -165,32 +245,6 @@ const MobileNavbar = () => {
                                                 </button>
                                             </div>
                                         </div>
-
-                                        <div className='divider my-4'></div>
-                                        <div className="counter-item">
-                                            <div className='flex flex-col'>
-                                                <span className="counter-heading">Pets</span>
-                                                <span className="counter-criteria">Bringing a service animal?</span>
-                                            </div>
-                                            <div className="counter-btn-container">
-                                                <button
-                                                disabled={options.pets <= 0}
-                                                className="counter-btn"
-                                                onClick={() => handleOption("pets", "d")}
-                                                >
-                                                    <p>-</p>
-                                                </button>
-                                                <span className="counter-number">
-                                                {options.pets}
-                                                </span>
-                                                <button
-                                                className="counter-btn"
-                                                onClick={() => handleOption("pets", "i")}
-                                                >
-                                                    <p>+</p>
-                                                </button>
-                                            </div>
-                                        </div>
                                     </div>
                                     :
                                     <div className='options-item'>
@@ -200,7 +254,6 @@ const MobileNavbar = () => {
                                 }
                                 
                             </div>
-                            
                         </div>
                         <div className="bottom-content" >
                             <div className='bottom-container' >
@@ -211,8 +264,14 @@ const MobileNavbar = () => {
                             </div>
                         </div>
                     </div>
-                    
                 </div>
+                :
+                null
+            }
+            {
+                open
+                ?
+                <FilterModal open={open} setOpen={setOpen}/>
                 :
                 null
             }
